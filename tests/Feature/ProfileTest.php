964586c40948b4c5233320e2927501f49cbaa636
Rodrigo <rodrigo-demolidor@hotmail.com>
2017-06-13 12:48:45 -0300

    Criado perfil do user

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -8,11 +8,22 @@ class ProfileTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /** @thread */
+    /** @test */
     function a_user_has_a_profile(){
       $user = create('App\User');
-      
-      $this->get("/profiles/{{$user->name}}")
+
+      $this->get("/profiles/{$user->name}")
            ->assertSee($user->name);
+    }
+
+    /** @test */
+    function profiles_display_all_threads_created_by_the_associated_user(){
+      $user = create('App\User');
+
+      $thread = create('App\Thread', ['user_id'=>$user->id]);
+
+      $this->get("/profiles/{$user->name}")
+           ->assertSee($thread->title)
+           ->assertSee($thread->body);
     }
 }
